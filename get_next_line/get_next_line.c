051de6cd542c137e2144499e206dd5e7388466aa
Achraf <80591781+skullknight99@users.noreply.github.com>
2021-11-30 13:38:16 +0100

    For Zyzz

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,79 +12,88 @@
 
 #include "get_next_line.h"
 
-static int	locate_char(char *s, char c)
+#ifndef	BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+#endif
+
+char	*str_ret(char *stc_arr)
 {
 	int	i;
 
 	i = 0;
-	while (s[i++])
-		if (s[i] == c)
-			return (i);
-	return (0);
-}
-
-static char	*get_one_line(char *s)
-{
-	char	*res;
-
-	res = ft_substr(s, 0, locate_char(s, '\n'));
-	return (res);
-}
-
-char	*ft_strdup(char *s1)
-{
-	char	*target;
-	size_t	i;
-
-	if (!s1)
-		return (NULL);
-	target = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
-	if (!(target))
+	if (!stc_arr[i])
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (stc_arr[i] && stc_arr[i] != '\n')
+		i++;
+	return (ft_substr(stc_arr, 0, i + 1));
+}
+
+char	*str_remainder(char *stc_arr)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!stc_arr)
+		return (NULL);
+	while (stc_arr[i])
 	{
-		target[i] = s1[i];
+		if (stc_arr[i] == '\n')
+		{
+			str = ft_substr(stc_arr, i + 1, ft_strlen(stc_arr));
+			free(stc_arr);
+			return (str);
+		}
 		i++;
 	}
-	target[i] = '\0';
-	return (target);
+	free (stc_arr);
+	return (NULL);
+}
+
+int	find_nl(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
+	static char	*stc_arr;
 	char		*buffer;
-	static char	*str;
 	char		*line;
 	int			i;
 
-	if (!str)
-		str = malloc(1);
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (!buffer)
+	line = NULL;
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	i = 1;
-	while (i && !locate_char(str, '\n'))
+	buffer = malloc(BUFFER_SIZE + 1);
+	while (!(find_nl(stc_arr)) && (i))
 	{
 		i = read(fd, buffer, BUFFER_SIZE);
-		if (i <= 0)
+		if (i < 0)
 		{
-			if (!*str)
-				return (free(buffer), NULL);
-			else
-			{
-				line = ft_strdup(str);
-				str = NULL;
-				return (line);
-			}
+			free(buffer);
+			return (NULL);
 		}
 		buffer[i] = '\0';
-		str = ft_strjoin(str, buffer);
+		stc_arr = ft_strjoin(stc_arr, buffer);
 	}
-	line = get_one_line(str);
-	str = ft_substr(str, locate_char(str, '\n') + 1,
-			ft_strlen(str) - locate_char(str, '\n'));
 	free(buffer);
+	line = str_ret(stc_arr);
+	stc_arr = str_remainder(stc_arr);
 	return (line);
 }
 
